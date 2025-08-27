e1b2dfea83fed3c53f646c5f1e4233860500a820
Muzamil Shiraz <bcs230302211@imsciences.edu.pk>
2025-08-27 08:00:03 +0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Navbar from './Components/Navbar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [bgVisible, setBgVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setBgVisible(true);
+    }, 100);
+  }, []);
+
   return (
-    <div className="fixed inset-0 min-h-screen w-screen bg-black">
+    <div
+      className={`fixed inset-0 min-h-screen w-screen transition-colors duration-1000 ${bgVisible ? 'bg-black' : 'bg-transparent'}`}
+    >
       <Navbar />
       {/* Other sections */}
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,18 @@
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 bg-black shadow-lg">
-      <div className="mx-auto flex max-w-6xl items-center justify-between px-8 py-4">
-        <div className="mt-3 pl-8 font-mono text-3xl font-extrabold tracking-wide text-white">
+    <nav className="sticky top-0 z-50 border-none shadow-none">
+      <div className="mt-1 mb-2 flex w-full items-center justify-between py-4 md:mt-2 lg:mt-3">
+        <a
+          href="/"
+          className="font-poppins ml-4 text-2xl font-extrabold tracking-wide text-white md:ml-24 lg:ml-56 lg:text-3xl"
+        >
           Muzamil Shiraz
-        </div>
-        <ul className="flex space-x-8">
+        </a>
+        <ul className="mr-4 hidden gap-x-5 space-x-8 text-lg font-normal md:mr-16 lg:mr-65 lg:flex">
           {['Home', 'About', 'Resume', 'Projects', 'Contact'].map((link) => (
             <li
               key={link}
-              className="cursor-pointer font-bold text-white hover:text-yellow-400"
+              className="cursor-pointer font-normal text-white hover:text-yellow-400"
             >
               {link}
             </li>
